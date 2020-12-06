405191aec08c30f5593f912dac27c19534c5223c
kmariah <kerriemdurham@gmail.com>
2020-12-06 17:54:54 +0800

    First commit

---
+++ CarApp/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace CarApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bushes1 = new System.Windows.Forms.PictureBox();
+            this.bushes2 = new System.Windows.Forms.PictureBox();
+            this.bushes3 = new System.Windows.Forms.PictureBox();
+            this.shrub1 = new System.Windows.Forms.PictureBox();
+            this.shrub2 = new System.Windows.Forms.PictureBox();
+            this.shrub3 = new System.Windows.Forms.PictureBox();
+            this.goose = new System.Windows.Forms.PictureBox();
+            this.bear1 = new System.Windows.Forms.PictureBox();
+            this.bear3 = new System.Windows.Forms.PictureBox();
+            this.bear2 = new System.Windows.Forms.PictureBox();
+            this.over = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bushes1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bushes2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bushes3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shrub1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shrub2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shrub3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bear1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bear3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bear2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Navy;
+            this.pictureBox6.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 936);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Navy;
+            this.pictureBox7.Location = new System.Drawing.Point(762, 0);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(10, 936);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bushes1
+            // 
+            this.bushes1.Image = ((System.Drawing.Image)(resources.GetObject("bushes1.Image")));
+            this.bushes1.Location = new System.Drawing.Point(447, 121);
+            this.bushes1.Name = "bushes1";
+            this.bushes1.Size = new System.Drawing.Size(164, 142);
+            this.bushes1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bushes1.TabIndex = 8;
+            this.bushes1.TabStop = false;
+            // 
+            // bushes2
+            // 
+            this.bushes2.Image = ((System.Drawing.Image)(resources.GetObject("bushes2.Image")));
+            this.bushes2.Location = new System.Drawing.Point(86, 294);
+            this.bushes2.Name = "bushes2";
+            this.bushes2.Size = new System.Drawing.Size(172, 157);
+            this.bushes2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bushes2.TabIndex = 9;
+            this.bushes2.TabStop = false;
+            // 
+            // bushes3
+            // 
+            this.bushes3.Image = ((System.Drawing.Image)(resources.GetObject("bushes3.Image")));
+            this.bushes3.Location = new System.Drawing.Point(536, 410);
+            this.bushes3.Name = "bushes3";
+            this.bushes3.Size = new System.Drawing.Size(158, 192);
+            this.bushes3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bushes3.TabIndex = 10;
+            this.bushes3.TabStop = false;
+            // 
+            // shrub1
+            // 
+            this.shrub1.Image = ((System.Drawing.Image)(resources.GetObject("shrub1.Image")));
+            this.shrub1.Location = new System.Drawing.Point(340, 698);
+            this.shrub1.Name = "shrub1";
+            this.shrub1.Size = new System.Drawing.Size(116, 93);
+            this.shrub1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.shrub1.TabIndex = 11;
+            this.shrub1.TabStop = false;
+            // 
+            // shrub2
+            // 
+            this.shrub2.Image = ((System.Drawing.Image)(resources.GetObject("shrub2.Image")));
+            this.shrub2.Location = new System.Drawing.Point(462, 698);
+            this.shrub2.Name = "shrub2";
+            this.shrub2.Size = new System.Drawing.Size(116, 93);
+            this.shrub2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.shrub2.TabIndex = 12;
+            this.shrub2.TabStop = false;
+            // 
+            // shrub3
+            // 
+            this.shrub3.Image = ((System.Drawing.Image)(resources.GetObject("shrub3.Image")));
+            this.shrub3.Location = new System.Drawing.Point(414, 779);
+            this.shrub3.Name = "shrub3";
+            this.shrub3.Size = new System.Drawing.Size(116, 93);
+            this.shrub3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.shrub3.TabIndex = 13;
+            this.shrub3.TabStop = false;
+            // 
+            // goose
+            // 
+            this.goose.Image = ((System.Drawing.Image)(resources.GetObject("goose.Image")));
+            this.goose.Location = new System.Drawing.Point(121, 582);
+            this.goose.Name = "goose";
+            this.goose.Size = new System.Drawing.Size(126, 133);
+            this.goose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.goose.TabIndex = 14;
+            this.goose.TabStop = false;
+            // 
+            // bear1
+            // 
+            this.bear1.Image = ((System.Drawing.Image)(resources.GetObject("bear1.Image")));
+            this.bear1.Location = new System.Drawing.Point(43, 164);
+            this.bear1.Name = "bear1";
+            this.bear1.Size = new System.Drawing.Size(126, 133);
+            this.bear1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bear1.TabIndex = 15;
+            this.bear1.TabStop = false;
+            // 
+            // bear3
+            // 
+            this.bear3.Image = ((System.Drawing.Image)(resources.GetObject("bear3.Image")));
+            this.bear3.Location = new System.Drawing.Point(568, 269);
+            this.bear3.Name = "bear3";
+            this.bear3.Size = new System.Drawing.Size(126, 133);
+            this.bear3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bear3.TabIndex = 16;
+            this.bear3.TabStop = false;
+            // 
+            // bear2
+            // 
+            this.bear2.Image = ((System.Drawing.Image)(resources.GetObject("bear2.Image")));
+            this.bear2.Location = new System.Drawing.Point(404, 0);
+            this.bear2.Name = "bear2";
+            this.bear2.Size = new System.Drawing.Size(126, 133);
+            this.bear2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bear2.TabIndex = 17;
+            this.bear2.TabStop = false;
+            // 
+            // over
+            // 
+            this.over.AutoSize = true;
+            this.over.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.over.ForeColor = System.Drawing.Color.Black;
+            this.over.Location = new System.Drawing.Point(265, 453);
+            this.over.Name = "over";
+            this.over.Size = new System.Drawing.Size(265, 51);
+            this.over.TabIndex = 19;
+            this.over.Text = "Game Over!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(774, 929);
+            this.Controls.Add(this.over);
+            this.Controls.Add(this.bear2);
+            this.Controls.Add(this.bear3);
+            this.Controls.Add(this.bear1);
+            this.Controls.Add(this.goose);
+            this.Controls.Add(this.shrub3);
+            this.Controls.Add(this.shrub2);
+            this.Controls.Add(this.shrub1);
+            this.Controls.Add(this.bushes3);
+            this.Controls.Add(this.bushes2);
+            this.Controls.Add(this.bushes1);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bushes1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bushes2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bushes3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shrub1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shrub2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shrub3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bear1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bear3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bear2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox bushes1;
+        private System.Windows.Forms.PictureBox bushes2;
+        private System.Windows.Forms.PictureBox bushes3;
+        private System.Windows.Forms.PictureBox shrub1;
+        private System.Windows.Forms.PictureBox shrub2;
+        private System.Windows.Forms.PictureBox shrub3;
+        private System.Windows.Forms.PictureBox goose;
+        private System.Windows.Forms.PictureBox bear1;
+        private System.Windows.Forms.PictureBox bear3;
+        private System.Windows.Forms.PictureBox bear2;
+        private System.Windows.Forms.Label over;
+    }
+}
+
---
+++ CarApp/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            over.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveFauna(gamespeed);
+            enemy(gamespeed + 1);
+            gameover();
+        }
+
+        int gamespeed = 0; 
+
+        void moveFauna(int speed)
+        {
+
+            if (bushes1.Top >= 500)
+            {
+                bushes1.Top = 0;
+            }
+            else
+            {
+                bushes1.Top += speed;
+            }
+
+            if (bushes2.Top >= 500)
+            {
+                bushes2.Top = 0;
+            } else
+            {
+                bushes2.Top += speed;
+            }
+
+            if (bushes3.Top >= 500)
+            {
+                bushes3.Top = 0;
+            }
+            else
+            {
+                bushes3.Top += speed;
+            }
+
+            if (shrub1.Top >= 500)
+            {
+                shrub1.Top = 0;
+            }
+            else
+            {
+                shrub1.Top += speed;
+            }
+
+            if (shrub2.Top >= 500)
+            {
+                shrub2.Top = 0;
+            }
+            else
+            {
+                shrub2.Top += speed;
+            }
+
+            if (shrub3.Top >= 500)
+            {
+                shrub3.Top = 0;
+            }
+            else
+            {
+                shrub3.Top += speed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                if(goose.Left>0)
+                {
+                    goose.Left += -9;
+                }
+            }
+            if(e.KeyCode==Keys.Right)
+            {
+                if(goose.Right<380)
+                {
+                    goose.Left += 9;
+                }
+            }
+
+            if(e.KeyCode==Keys.Up)
+            {
+                if(gamespeed < 21)
+                {
+                    gamespeed++;
+                }
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                if(gamespeed > 0)
+                {
+                    gamespeed--;
+                }
+            }
+        }
+
+        Random r = new Random();
+        int x, y;
+        void enemy(int speed)
+        {
+            if (bear1.Top >= 500)
+            {
+                x = r.Next(0, 380);
+                bear1.Location = new Point(x, 0);
+            }
+            else
+            {
+                bear1.Top += speed;
+            }
+
+            if (bear2.Top >= 500)
+            {
+                x = r.Next(0, 380);
+                bear2.Location = new Point(x, 0);
+            }
+            else
+            {
+                bear2.Top += speed;
+            }
+
+            if (bear3.Top >= 500)
+            {
+                x = r.Next(0, 380);
+                bear3.Location = new Point(x, 0);
+            }
+            else
+            {
+                bear3.Top += speed;
+            }
+        }
+
+        void gameover()
+        {
+            if(goose.Bounds.IntersectsWith(bear1.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+            if (goose.Bounds.IntersectsWith(bear2.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+            if (goose.Bounds.IntersectsWith(bear3.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true;
+            }
+        }
+    }
+}
